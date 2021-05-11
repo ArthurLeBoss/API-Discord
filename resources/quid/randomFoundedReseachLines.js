cf756644c5b0142f7e233cd824be42fe38294902
ArthurLeBoss <56627445+ArthurLeBoss@users.noreply.github.com>
2021-05-11 23:14:15 +0200

    update

--- resources/quid/randomFoundedReseachLines.js
+++
@@ -1,8 +0,0 @@
-const randomFoundedReseachLines = [
-    ", I have found something !",
-    ", that is for you :",
-    ", enjoy !",
-    ", is it yours ?",
-    ", wtf r u doing with this ?",
-]
-module.exports = { randomFoundedReseachLines };
